777b0703a0ecb22f585c6a910c77a7cb290cd4f9
tiendat <dat.70.k3@gmail.com>
2024-12-10 10:32:20 +0700

    bai 3

---
+++ ss12_03.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int tinhGiaiThua(int n) {
+    int giaiThua = 1;
+    for (int i = 1; i <= n; i++) {
+        giaiThua *= i;
+    }
+    return giaiThua;
+}
+
+int main() {
+    int so;
+
+    printf("Nhap mot so nguyen: ");
+    scanf("%d", &so);
+
+    if (so < 0) {
+        printf("Khong the tinh giai thua cua so am.\n");
+    } else {
+        printf("Giai thua cua %d la: %d\n", so, tinhGiaiThua(so));
+    }
+
+    return 0;
+}
+
