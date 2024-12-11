044865330aef683a7ecf6b3d4da019b55d334183
tiendat <dat.70.k3@gmail.com>
2024-12-11 10:37:53 +0700

    bai 8

---
+++ s12_08.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int timUCLN(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &num1);
+    printf("Nhap so thu hai: ");
+    scanf("%d", &num2);
+
+    int ucln = timUCLN(num1, num2);
+
+    printf("Uoc chung lon nhat cua %d va %d la: %d\n", num1, num2, ucln);
+
+    return 0;
+}
+
